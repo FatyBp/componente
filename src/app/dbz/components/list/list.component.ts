918c24d0fc23d0f3aed622f947a7f4a2ddd8bdc4
Fatybp <fatimabenitez796@gmail.com>
2023-05-15 21:16:56 -0600

    componentes

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { Personaje } from '../../interfaces/persona.interface';
+
+@Component({
+  selector: 'app-dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+  @Input()
+  public personaList : Personaje[]=[
+    {
+    nombre : "Vegeta",
+    poder : 20000
+  },
+];
+}
